b276550779917fae517158eed1d53405f6cf0bec
ramuunnava <ramuunnava1@gmail.com>
2022-11-02 18:15:06 -0500

    added

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 unnava ramu
 hosted remote render server <https://f2wb13unnava.onrender.com>
 
-
-
-
- ~ class mercedes benz { String mercedes_benz_luxurious_name; var mercedes_benz_High_Beam_type ; number mercedes_benz_tire_size; } ~
+myclass=car
+my attributes are
+manufacturer:(String)
+model:(String)
+price:(Number)
--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var carRouter = require('./routes/car');
 var selectorRouter = require('./routes/selector');
-var addmodsRouter = require('./routes/addmods');
+var gridbuildRouter = require('./routes/gridbuild');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/car', carRouter);
 app.use('/selector', selectorRouter);
-app.use('/addmods', addmodsRouter);
+app.use('/gridbuild', gridbuildRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
